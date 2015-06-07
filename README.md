ac56543ced7cdfa84fd6464c1d4370bb1d999de9
Chunliang Lyu <hi@chunlianglyu.com>
2015-06-07 16:54:20 +0800

    merge the website repo

---
+++ README.md
@@ -0,0 +1,18 @@
+# SwitchPal Official Website
+
+To start development:
+
+    $ npm install -g bower gulp
+    $ bower install && npm install
+
+To build to `dist` folder:
+
+    $ gulp build
+
+To deploy to GitHub Pages:
+
+    $ gulp deploy
+
+## Reference
+
+- Based on the [StartBootstrap Creative Theme](https://github.com/IronSummitMedia/startbootstrap-creative)
---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+var gulp = require('gulp');
+var useref = require('gulp-useref');
+var ghPages = require('gulp-gh-pages');
+
+gulp.task('assets', function () {
+  return gulp.src([
+    'css/**/*.*',
+    'js/**/*.*',
+    'img/**/*.*'
+  ], {base: './'})
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('fonts', function () {
+  return gulp.src('bower_components/fontawesome/fonts/**/*.*')
+    .pipe(gulp.dest('dist/fonts'));
+});
+
+gulp.task('html', function () {
+  var assets = useref.assets();
+
+  return gulp.src('index.html')
+    .pipe(assets)
+    .pipe(assets.restore())
+    .pipe(useref())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('build', ['assets', 'fonts', 'html'], function () {
+});
+
+gulp.task('cname', function () {
+  return gulp.src('CNAME')
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('deploy', ['build', 'cname'], function() {
+  return gulp.src('./dist/**/*')
+    .pipe(ghPages({remoteUrl: 'git@github.com:cllu/getswitchpal.com.git'}));
+});
